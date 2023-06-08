a1f53ccccd76f4e0a21c64dbad7bf5f289b3352f
Ann956 <85205656+Ann956@users.noreply.github.com>
2023-06-08 16:57:47 +0800

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,19 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-def read( word ):
-    url = f'https://dict.idioms.moe.edu.tw/idiomList.jsp?idiom={word}&qMd=0&qTp=1&qTp=2#searchL'
-    
-    html = requests.get( url )
-    bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('table', id='searchL')
-   
-    try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
-        phone = [e.text for e in phones]
-        s = " ".join( phone )
-        # s = row.find('sub')
-        print( chinese+ s )
-    except:
-        print( '查無此成語' )
+url = f'https://pedia.cloud.edu.tw/Entry/Detail?title={word}&search={word}#searchL'
+html = requests.get( url )
+bs = BeautifulSoup(html.text,'lxml')
+data = bs.find('span', class_='withoutRefLink')
+data.text
