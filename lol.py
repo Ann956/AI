828dc293bfc6ea43a5127d6c926e29be9211b654
Ann956 <85205656+Ann956@users.noreply.github.com>
2023-06-08 17:17:02 +0800

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-url = f'https://pedia.cloud.edu.tw/Entry/Detail?title={word}&search={word}#searchL'
-html = requests.get( url )
-bs = BeautifulSoup(html.text,'lxml')
-data = bs.find('span', class_='withoutRefLink')
-return(data.text)
+def read( word ):
+  url = f'https://pedia.cloud.edu.tw/Entry/Detail?title={word}&search={word}#searchL'
+  html = requests.get( url )
+  bs = BeautifulSoup(html.text,'lxml')
+  data = bs.find('span', class_='withoutRefLink')
+  return(data.text)
